7cf2e728cae83945d235e07663f7a80a9bbe3f32
Nicholas Choong <nicholascwc1997@gmail.com>
2021-10-21 21:04:37 +0800

    Fixed class name

--- s22412148/baye_behaviour_agent.py
+++ s22412148/baye_behaviour_agent.py
@@ -3,7 +3,7 @@ import math, random
 from itertools import combinations
 import collections
 
-class baye_behaviour_agent(Agent):
+class BayeBehaviourAgent(Agent):
     def __init__(self, name='22412148'):
         self.name = name
 
